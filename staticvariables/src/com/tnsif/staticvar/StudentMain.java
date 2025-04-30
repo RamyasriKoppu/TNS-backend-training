77bdb0866f78b0967a8fb59598f35f5ad0f57c7d
HOME <HOME@DESKTOP-ON9UP33>
2025-04-30 12:27:00 -0400

    first commit

---
+++ staticvariables/src/com/tnsif/staticvar/StudentMain.java
@@ -0,0 +1,21 @@
+package com.tnsif.staticvar;
+
+public class StudentMain {
+	public static void main(String[] args) {
+	     Student ob=new Student();
+	     ob.sid=101;
+	     ob.sname="ram";
+	     ob.display();
+	    
+	     Student o=new Student();
+	     o.sid=102;
+	     o.sname="laxman";
+	     o.display();
+	    
+	     Student b=new Student();
+	     b.sid=103;
+	     b.sname="seetha";
+	     b.display();	
+		}
+
+}
